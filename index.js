78f0f8d0b2b52414afc4cc5f96c1b5f7399b719a
junaidali40232-cmd <junaidali40232@gmail.com>
2026-03-12 16:08:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const PORT = process.env.PORT || 3000;
 
 // --- IMPORT ALL PANELS ---
 const junaid = require("./api/junaid");
+const njunaid = require("./api/njunaid");
 
 // --- ROUTES ---
 app.use("/api/junaid", junaid);
+app.use("/api/njunaid", njunaid);
+
 
 // --- HEALTH CHECK ---
 app.get("/", (req,res)=> res.send("API RUNNING ✅"));
